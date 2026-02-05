fad05586c9e22c24e6f6a0b42d70adb900e5009f
gangaraju2005 <karaganagangaraju2005@gmail.com>
2026-02-05 10:35:27 +0530

    Update subnet_id to use first element of array

--- modules/main.tf
+++ modules/main.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "jin" {
     ami           = var.ami
     instance_type = var.instance_type
-    subnet_id     = aws_subnet.na.id
-    availability_zone = var.subnet_az
+    subnet_id     = aws_subnet.na[0].id
     vpc_security_group_ids = [aws_security_group.sg.id]
   
 }
